3bbc7d18d26b309fd61aa6e88eb8433d960da76a
yasmin crisostomo <yasmin.crisostomo@mobile2you.com.br>
2023-02-19 16:54:22 -0300

    updating elements

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,18 @@
 const apiKey = "9c5b2cefde8dc62dcfabc013abade5b6";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 
-const cityInput = document.querySelector("#city-input")
-const searchBtn = document.querySelector("#search")
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
 
-const cityElement = document.querySelector("#city")
-const tempElement = document.querySelector("#temperature span")
-const descElement = document.querySelector("#description")
-const weatherIconElement = document.querySelector("#weather-icon")
-const countryElement = document.querySelector("#country")
-const umidityElement = document.querySelector("#umidity span")
-const windElement = document.querySelector("#wind span")
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const humidityElement = document.querySelector("#humidity span");
+const windElement = document.querySelector("#wind span");
 
+const weatherContainer = document.querySelector("#weather-data");
 // Funções
 
 const getWeatherData = async (city) => {
@@ -21,11 +22,21 @@ const getWeatherData = async (city) => {
   const res = await fetch(apiWeatherURL);
   const data = await res.json();
 
-  console.log(data);
+  return data
 }
 
-const showWeatherData = (city) => {
-  getWeatherData(city);
+const showWeatherData = async (city) => {
+  const data = await getWeatherData(city);
+
+  cityElement.innerText = data.name;
+  tempElement.innerText = parseInt(data.main.temp);
+  descElement.innerText = data.weather[0].description;
+  weatherIconElement.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+  countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+  humidityElement.innerText = `${data.main.humidity}%`;
+  windElement.innerText = `${data.wind.speed}km/h`;
+
+  weatherContainer.classList.remove("hide")
 }
 
 // Eventos
@@ -36,4 +47,12 @@ searchBtn.addEventListener("click", (e) => {
   const city = cityInput.value;
 
   showWeatherData(city)
+})
+
+cityInput.addEventListener("keyup", (e) => {
+  if(e.code === "Enter") {
+    const city = e.target.value
+
+    showWeatherData(city);
+  }
 })
